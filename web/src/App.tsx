d960ee086c8389edfdce45cc3c2e866b42d04821
pedrohenriquebraga <gamesantos7@gmail.com>
2020-10-12 11:46:32 -0300

    First commit

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/global.css'
+import Routes from './routes'
+
+// JSX - JavaScript XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
