ba63674ce324685feda907564808d68fa8cb1a21
pedrohenriquebraga <gamesantos7@gmail.com>
2020-10-18 11:28:19 -0300

    Add animation in loading screen

--- mobile/src/components/loading.tsx
+++ mobile/src/components/loading.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import Lottie from 'lottie-react-native'
+
+import LoadingAnimation from '../animations/loading-animation.json'
 
 export default function Loading() {
     return (
         <View style={styles.Container}>
+            <Lottie source={LoadingAnimation} loop autoPlay />
             <Text style={styles.LoadingText}>Buscando o orfanato...</Text>
         </View>
     )
@@ -13,13 +17,14 @@ const styles = StyleSheet.create({
     Container: {
         width: '100%',
         height: '100%',
-        justifyContent: "center",
+        justifyContent: "space-between",
         alignItems: "center"
     },
 
     LoadingText: {
         fontSize: 24,
         fontFamily: "nunito700",
-        color: '#15b6d6'
+        color: '#15b6d6',
+        marginTop: 200
     }
 })
